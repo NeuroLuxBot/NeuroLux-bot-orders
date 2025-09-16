d81a061f8ddec16cf829022c55c1318e43e980a4
NeuroLuxBot <alekseyvorona0708@gmail.com>
2025-09-16 10:25:10 +0500

    Update counter.py

--- counter.py
+++ counter.py
@@ -1,16 +1,13 @@
 import os
 
-def increment_counter(counter_file="counter.txt"):
-    # Создаём файл, если его нет
+def increment_counter(counter_file="counter_orders.txt"):
     if not os.path.exists(counter_file):
         with open(counter_file, "w") as f:
             f.write("0")
 
-    # Считываем текущее значение
     with open(counter_file, "r") as f:
         count = int(f.read())
 
-    # Увеличиваем и сохраняем
     count += 1
     with open(counter_file, "w") as f:
         f.write(str(count))
